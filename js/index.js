df893326433393f932e2d6404bed93205700a32d
yankev07 <kevin.yanogo@gmail.com>
2018-01-22 14:10:46 +0800

    Required Input Fields Added!!
    Required Input Fields Added!!

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
